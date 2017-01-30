5ed1bab615be970de11a2e84d471d684da5c5771
Yash Sharma <95yashsharma@gmail.com>
2017-01-30 13:03:43 +0530

    completed

--- js/index.js
+++ js/index.js
@@ -1,30 +1,32 @@
 $(function(){
 
-	var all_countries=[];
+	var all_countries; 	
 	$.ajax({
 		url:"https://restcountries.eu/rest/v1/all",
 		success:function(response){
 			all_countries = response;
+			// console.log(all_countries);
+			countries=[]; 
+			$.each(all_countries,function(index,value){
+			countries.push(all_countries[index].name);
+			});
+
+			var country = $("#country");
+			var list = "" ;
+			$.each(countries,function(index,value){
+				list+="<option>"+value+"</option>";
+			});
+			country.append(list);
 		}
 	});
-	countries=[]; 
-	$.each(all_countries,function(index,value){
-		countries.push(all_countries[index].name);
-	});
+	
 
-	var country = $("#country");
-	var list = "" ;
-	$.each(countries,function(index,value){
-		list+="<option>"+value+"</option>";
-	});
-	country.append(list);
-
-	var nationality = ["Indian","1","2","3"] ;
-	var nation = $("#nation");
-	var list = ""; 
-	$.each(nationality,function(index,value){
-		list+="<option>"+value+"</option>";
-	});
-	nation.append(list);
+	// var nationality = ["Indian","1","2","3"] ;
+	// var nation = $("#nation");
+	// var list = ""; 
+	// $.each(nationality,function(index,value){
+	// 	list+="<option>"+value+"</option>";
+	// });
+	// nation.append(list);
 
 });
